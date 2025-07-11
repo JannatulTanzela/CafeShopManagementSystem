8f2ffd7b4d908509e0744b2a60f03c4409346841
JannatulTanzela <jftanzela@gmail.com>
2025-07-11 21:56:50 +0600

    maiform controller

--- CafeShop/src/cafeshop/FXMLDocumentController.java
+++ CafeShop/src/cafeshop/FXMLDocumentController.java
@@ -35,7 +35,6 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-
 /**
  *
  * @author Jannatul Tanzela
@@ -82,8 +81,7 @@ public class FXMLDocumentController implements Initializable {
     private AnchorPane loginForm1;
     @FXML
     private Button loginBtn1;
-    
-    
+
     @FXML
     public void loginBtn() {
 
@@ -100,8 +98,6 @@ public class FXMLDocumentController implements Initializable {
             connect = database.connectDB();
 
             try {
-                
-                
                 prepare = connect.prepareStatement(selectData);
                 prepare.setString(1, username.getText());
                 prepare.setString(2, password.getText());
@@ -109,26 +105,33 @@ public class FXMLDocumentController implements Initializable {
                 Result = prepare.executeQuery();
 
                 if (Result.next()) {
+                    
+                    data.username1 = username.getText();
+                    
+                    
                     alert = new Alert(AlertType.INFORMATION);
                     alert.setTitle("Information Message");
                     alert.setHeaderText(null);
                     alert.setContentText("Successfully Login ");
                     alert.showAndWait();
                     
-                   Parent root = FXMLLoader.load(getClass().getResource("mainForm.fxml")) ;
-                   
-                   Stage stage = new Stage();
-                   Scene scene = new Scene(root);
-                   
-                   stage.setTitle("Cafe Shop Management System");
-                   stage.setMinWidth(1100);
-                   stage.setMinHeight(600);
-                   
-                   stage.setScene(scene);
-                   stage.show();
-                   
-                   loginBtn.getScene().getWindow().hide();
-                   
+                    
+                    
+
+                    Parent root = FXMLLoader.load(getClass().getResource("mainForm.fxml"));
+
+                    Stage stage = new Stage();
+                    Scene scene = new Scene(root);
+
+                    stage.setTitle("Cafe Shop Management System");
+                    stage.setMinWidth(1100);
+                    stage.setMinHeight(600);
+
+                    stage.setScene(scene);
+                    stage.show();
+
+                    loginBtn.getScene().getWindow().hide();
+                    
                     
                     
                     
@@ -165,60 +168,60 @@ public class FXMLDocumentController implements Initializable {
             connect = database.connectDB();
 
             try {
-                
-                String checkUsername = "SELECT usernameFrom employee WHERE username ='" + username_2.getText() + "'";
-                prepare =connect.prepareStatement(checkUsername);
+
+                String checkUsername = "SELECT username From employee WHERE username ='" + username_2.getText() + "'";
+                prepare = connect.prepareStatement(checkUsername);
                 Result = prepare.executeQuery();
-                
-                if(Result.next()){
+
+                if (Result.next()) {
                     alert = new Alert(AlertType.ERROR);
                     alert.setTitle("Error Message");
                     alert.setHeaderText(null);
-                    alert.setContentText(username_2.getText()+ " is already taken");
+                    alert.setContentText(username_2.getText() + " is already taken");
                     alert.showAndWait();
-                }else if (password_2.getText().length ()< 8){
+                } else if (password_2.getText().length() < 8) {
                     alert = new Alert(AlertType.ERROR);
                     alert.setTitle("Error Message");
                     alert.setHeaderText(null);
                     alert.setContentText("Invalid Password,atleast 8 characters are needed");
                     alert.showAndWait();
-                }else{
-                    
-                prepare = (PreparedStatement) connect.prepareStatement(regData);
-                prepare.setString(1, username_2.getText());
-                prepare.setString(2, password_2.getText());
-                prepare.setString(3, (String) question.getSelectionModel().getSelectedItem());
-                prepare.setString(4, answer.getText());
+                } else {
 
-                prepare.executeUpdate();
+                    prepare = (PreparedStatement) connect.prepareStatement(regData);
+                    prepare.setString(1, username_2.getText());
+                    prepare.setString(2, password_2.getText());
+                    prepare.setString(3, (String) question.getSelectionModel().getSelectedItem());
+                    prepare.setString(4, answer.getText());
 
-                alert = new Alert(AlertType.INFORMATION);
-                alert.setTitle("Information Message");
-                alert.setHeaderText(null);
-                alert.setContentText("Successfully registered Acount");
-                alert.showAndWait();
+                    prepare.executeUpdate();
 
-                username_2.setText("");
-                password_2.setText("");
-                question.getSelectionModel().clearSelection();
-                answer.setText("");
+                    alert = new Alert(AlertType.INFORMATION);
+                    alert.setTitle("Information Message");
+                    alert.setHeaderText(null);
+                    alert.setContentText("Successfully registered Acount");
+                    alert.showAndWait();
 
-                TranslateTransition slider = new TranslateTransition();
+                    username_2.setText("");
+                    password_2.setText("");
+                    question.getSelectionModel().clearSelection();
+                    answer.setText("");
 
-                slider.setNode(sideForm);
-                slider.setToX(0);
-                slider.setDuration(Duration.seconds(.5));
+                    TranslateTransition slider = new TranslateTransition();
 
-                slider.setOnFinished((ActionEvent e) -> {
-                    alreadyHave.setVisible(false);
-                    sideCreateBtn.setVisible(true);
+                    slider.setNode(sideForm);
+                    slider.setToX(0);
+                    slider.setDuration(Duration.seconds(.5));
 
-                });
+                    slider.setOnFinished((ActionEvent e) -> {
+                        alreadyHave.setVisible(false);
+                        sideCreateBtn.setVisible(true);
 
-                slider.play();     
-          
+                    });
+
+                    slider.play();
+
                 }
-                               
+
             } catch (Exception e) {
                 e.printStackTrace();
             };
@@ -245,7 +248,7 @@ public class FXMLDocumentController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-         regLquestionList();
+        regLquestionList();
     }
 
     @FXML
--- CafeShop/src/cafeshop/MainFormController.java
+++ CafeShop/src/cafeshop/MainFormController.java
@@ -4,11 +4,17 @@
  */
 package cafeshop;
 
+import static cafeshop.data.username1;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 
 /**
  * FXML Controller class
@@ -27,15 +33,57 @@ public class MainFormController implements Initializable {
     private Button customers_btn;
     @FXML
     private Button logout_btn;
-
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+    private AnchorPane main_form;
+    @FXML
+    private AnchorPane inventory_form;
+    @FXML
+    private TableView<?> inventory_tableView;
+    @FXML
+    private TableColumn<?, ?> inventory_col_productID;
+    @FXML
+    private TableColumn<?, ?> inventory_col_productName;
+    @FXML
+    private TableColumn<?, ?> inventory_col_type;
+    @FXML
+    private TableColumn<?, ?> inventory_col_stock;
+    @FXML
+    private TableColumn<?, ?> inventory_col_price;
+    @FXML
+    private TableColumn<?, ?> inventory_col_status;
+    @FXML
+    private TableColumn<?, ?> inventory_col_date;
+    @FXML
+    private ImageView inventory_imageView;
+    @FXML
+    private Button inventory_importBtn;
+    @FXML
+    private Button inventory_addBtn;
+    @FXML
+    private Button inventory_updateBtn;
+    @FXML
+    private Button inventory_clearBtn;
+    @FXML
+    private Button inventory_deleteBtn;
+    @FXML
+    private Label username1;
     
+    
+    public void displayUsername(){
+        
+        String user = data.username1;
+        user = user.substring(0,1).toUpperCase() + user.substring(1);
+        
+        username1.setText(user);
+        
+        
+    }
+
+   
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        displayUsername();
     }    
     
 }
