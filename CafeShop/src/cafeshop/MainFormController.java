30b20e216d00ab277dfcec57f4ebe203f9dcc272
JannatulTanzela <jftanzela@gmail.com>
2025-07-13 01:19:12 +0600

    logout button add

--- CafeShop/src/cafeshop/MainFormController.java
+++ CafeShop/src/cafeshop/MainFormController.java
@@ -5,16 +5,33 @@
 package cafeshop;
 
 import static cafeshop.data.username1;
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.PreparedStatement;
+import com.mysql.jdbc.Statement;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -38,21 +55,21 @@ public class MainFormController implements Initializable {
     @FXML
     private AnchorPane inventory_form;
     @FXML
-    private TableView<?> inventory_tableView;
+    private TableView<productData> inventory_tableView;
     @FXML
-    private TableColumn<?, ?> inventory_col_productID;
+    private TableColumn<productData, String> inventory_col_productID;
     @FXML
-    private TableColumn<?, ?> inventory_col_productName;
+    private TableColumn<productData, String> inventory_col_productName;
     @FXML
-    private TableColumn<?, ?> inventory_col_type;
+    private TableColumn<productData, String> inventory_col_type;
     @FXML
-    private TableColumn<?, ?> inventory_col_stock;
+    private TableColumn<productData, String> inventory_col_stock;
     @FXML
-    private TableColumn<?, ?> inventory_col_price;
+    private TableColumn<productData, String> inventory_col_price;
     @FXML
-    private TableColumn<?, ?> inventory_col_status;
+    private TableColumn<productData, String> inventory_col_status;
     @FXML
-    private TableColumn<?, ?> inventory_col_date;
+    private TableColumn<productData, String> inventory_col_date;
     @FXML
     private ImageView inventory_imageView;
     @FXML
@@ -67,23 +84,156 @@ public class MainFormController implements Initializable {
     private Button inventory_deleteBtn;
     @FXML
     private Label username1;
-    
-    
-    public void displayUsername(){
-        
-        String user = data.username1;
-        user = user.substring(0,1).toUpperCase() + user.substring(1);
-        
-        username1.setText(user);
-        
-        
+    @FXML
+    private TextField inventory_productID;
+    @FXML
+    private TextField inventory_stock;
+    @FXML
+    private TextField inventory_productName;
+    @FXML
+    private TextField inventory_price;
+    @FXML
+    private ComboBox<?> inventory_status;
+    @FXML
+    private ComboBox<?> inventory_type;
+
+    private Alert alert;
+
+    private Connection connect;
+    private PreparedStatement prepare;
+    private Statement statement;
+    private ResultSet Result;
+
+    public ObservableList<productData> inventoryDataList() {
+
+        ObservableList<productData> listData = FXCollections.observableArrayList();
+        String sql = "SELECT * FROM product";
+
+        connect = database.connectDB();
+
+        try {
+            prepare = connect.prepareStatement(sql);
+            Result = prepare.executeQuery();
+
+            productData prodData;
+
+            while (Result.next()) {
+                prodData = new productData(
+                        result.getInt("id"),
+                        result.getString("productId"),
+                        result.getString("ProductName"),
+                        result.getString("type"),
+                        result.getInt("stock"),
+                        result.getDouble("price"),
+                        result.getString("status"),
+                        result.getString("image"),
+                        result.getDate("date"));
+
+                listData.add(prodData);
+
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
+        return listData;
+
+    }
+    private ObservableList<productData> inventoryListData;
+
+    public void InventoryShowData() {
+        inventoryListData = inventoryDataList();
+
+        inventory_col_productID.setCellValueFactory(new PropertyValueFactory<>("productId"));
+        inventory_col_productName.setCellValueFactory(new PropertyValueFactory<>("productName"));
+        inventory_col_type.setCellValueFactory(new PropertyValueFactory<>("type"));
+        inventory_col_stock.setCellValueFactory(new PropertyValueFactory<>("stock"));
+        inventory_col_price.setCellValueFactory(new PropertyValueFactory<>("price"));
+        inventory_col_status.setCellValueFactory(new PropertyValueFactory<>("status"));
+        inventory_col_date.setCellValueFactory(new PropertyValueFactory<>("date"));
+
+        inventory_tableView.setItems(inventoryListData);
+
     }
 
-   
-    
+    private String[] typeList = {"Meals", "Drinks"};
+
+    public void inventoryTypeList() {
+
+        List<String> typeL = new ArrayList<>();
+
+        for (String data : typeList) {
+            typeL.add(data);
+        }
+
+        ObservableList listData = FXCollections.observableArrayList(typeL);
+        inventory_type.setItems(listData);
+
+    }
+
+    private String[] statusList = {"Available", "Unavailable"};
+
+    public void inventoryStatusList() {
+
+        List<String> statusL = new ArrayList<>();
+
+        for (String data : statusList) {
+            statusL.add(data);
+        }
+
+        ObservableList listData = FXCollections.observableArrayList(statusL);
+        inventory_status.setItems(listData);
+
+    }
+
+    @FXML
+    public void logout() {
+        try {
+            alert = new Alert(AlertType.CONFIRMATION);
+            alert.setTitle("Error Message");
+            alert.setHeaderText(null);
+            alert.setContentText("Are you sure you want to log out?");
+            Optional<ButtonType> option = alert.showAndWait();
+
+            if (option.get().equals(ButtonType.OK)) {
+
+                logout_btn.getScene().getWindow().hide();
+
+                Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+
+                Stage stage = new Stage();
+                Scene scene = new Scene(root);
+
+                stage.setTitle("Cafe Shop Management System");
+
+                stage.setScene(scene);
+                stage.show();
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void displayUsername() {
+
+        String user = data.username1;
+        user = user.substring(0, 1).toUpperCase() + user.substring(1);
+
+        username1.setText(user);
+
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         displayUsername();
-    }    
-    
+
+        inventoryTypeList();
+
+        inventoryStatusList();
+        InventoryShowData();
+
+    }
+
 }
